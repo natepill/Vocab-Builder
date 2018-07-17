7d9ace6fb10d57dc0cbdfe17176b985a28961eac
Tushar Verma <tushar.m.verma@gmail.com>
2018-07-17 14:15:32 -0700

    Timer Added to the View Controller and Game View Controller

--- VocabBuilder/Controllers/GameViewController.swift
+++ VocabBuilder/Controllers/GameViewController.swift
@@ -12,7 +12,10 @@ class GameViewController: UIViewController, UITextFieldDelegate {
     
     var wordsUsed: [String] = []
     var words: String = "Words Used: " // Delete this later
-    
+    var gameInt = 11
+    var gameTimer = Timer()
+    @IBOutlet weak var resultLabel: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var letterLabel: UILabel!
     @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var wordInputTextField: UITextField!
@@ -25,6 +28,10 @@ class GameViewController: UIViewController, UITextFieldDelegate {
         
         self.wordInputTextField.delegate = self
         errorLabel.isHidden = true
+        // Starting the timer for the main game screen
+        gameTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(GameViewController.game), userInfo: nil, repeats: true)
+        // Popping up the keyboard as soon the GameViewController loads
+        wordInputTextField.becomeFirstResponder()
         
     }
     
@@ -43,9 +50,32 @@ class GameViewController: UIViewController, UITextFieldDelegate {
     }
 
 
+    @IBAction func resultButtonTapped(_ sender: UIButton) {
+        segueToResultViewController()
+    }
     
 }
 
+extension GameViewController{
+    @objc func game() {
+        // Decrementing the game timer by 1
+        gameInt -= 1
+        // After decrementing the timer update label to the current value
+        timeLabel.text = String(gameInt)
+        // Whenever the timer is equal to 0 stop the timer and go to the result screen
+        if gameInt == 0 {
+            // Stop the timer
+            gameTimer.invalidate()
+            // Go to the result view controller
+            segueToResultViewController()
+        }
+    }
+    
+    func segueToResultViewController(){
+        // Perform a segue to the next view controller
+        self.performSegue(withIdentifier: "resultScreenSegue", sender: self)
+    }
+}
 
 
 
