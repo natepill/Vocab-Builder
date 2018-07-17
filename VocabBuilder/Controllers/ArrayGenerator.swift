420935b5966aca626924471d31d7415a50304e7e
Owais Raza <owaisraza64@gmail.com>
2018-07-17 15:42:23 -0700

    Added Array Generator
    Added the random array generator struct

---
+++ VocabBuilder/Controllers/ArrayGenerator.swift
@@ -0,0 +1,25 @@
+//
+//  ArrayGenerator.swift
+//  VocabBuilder
+//
+//  Created by Owais Raza on 7/17/18.
+//  Copyright © 2018 Memo. All rights reserved.
+//
+
+import Foundation
+
+struct arrayGenerator {
+    
+    var alphabetArray: [String] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    
+    func generateRandomArray(alphabetArray: [String]) -> [String] {
+        var randomArray = [String]()
+        for _ in 1...26 {
+            let randomNumber = Int(arc4random_uniform(25))
+            randomArray.append(alphabetArray[randomNumber])
+        }
+        
+        return randomArray
+    }
+    
+}
