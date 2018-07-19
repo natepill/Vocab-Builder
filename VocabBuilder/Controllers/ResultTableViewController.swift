0bf9ac87ecf734e47b2c73feb5c7e9a8ee1b0221
Guillermo Ignacio Sanchez Lamas <sanguillermo98@gmail.com>
2018-07-19 16:15:08 -0700

    Finished first stable version
    - Shows related words now

--- VocabBuilder/Controllers/ResultTableViewController.swift
+++ VocabBuilder/Controllers/ResultTableViewController.swift
@@ -12,30 +12,54 @@ class ResultTableViewController: UIViewController {
     
     var score: Int = 0
     var wordsUsed: [String] = []
+    var wordsAndSynonyms: [String: String] = [:]
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var nameInputTextField: UITextField!
     @IBOutlet weak var resetButton: UIButton!
     
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         scoreLabel.text = String(score)
-        
-    
-        func synonyms(for word: String) {
-                print()
-        }
-        
+        getSynonyms()
         
     }
+
     
+    func getSynonyms() {
+        print("Testing synonyms")
+        
+        for word in wordsUsed {
+            var synonymText: String = ""
+            Thesaurus.getSynonymsFor(word: word.lowercased()) { (synonymsArray) in
+                DispatchQueue.main.sync {
+                    for synonym in synonymsArray {
+                        if synonym == synonymsArray[synonymsArray.count - 1] {
+                            synonymText += synonym
+                        } else {
+                            synonymText += synonym +  ", "
+                        }
+                    }
+                    print(synonymText)
+                    self.wordsAndSynonyms[word] = synonymText
+                    self.tableView.reloadData()
+                    
+                }
+            }
+        }
+    }
+    
+
+
 }
 
 
 extension ResultTableViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return wordsUsed.count
     }
@@ -43,7 +67,14 @@ extension ResultTableViewController: UITableViewDelegate, UITableViewDataSource 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ResultTableViewCell", for: indexPath) as! ResultTableViewCell
         cell.wordTitleLabel.text = wordsUsed[indexPath.row]
-        cell.descriptionTitleLabel.text = "[Synonyms]"
+        cell.descriptionTitleLabel.text = wordsAndSynonyms[wordsUsed[indexPath.row]]
         return cell
     }
+    
+    
 }
+
+
+
+
+
