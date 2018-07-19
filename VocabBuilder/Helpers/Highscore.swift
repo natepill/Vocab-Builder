8e94faf7119460d8a80d8d9b759bb15b93b3c5b6
Owais Raza <owaisraza64@gmail.com>
2018-07-19 13:11:24 -0700

    High Score  (1)

---
+++ VocabBuilder/Helpers/Highscore.swift
@@ -0,0 +1,21 @@
+//
+//  Highscore.swift
+//  VocabBuilder
+//
+//  Created by Owais Raza on 7/19/18.
+//  Copyright © 2018 Memo. All rights reserved.
+//
+
+import Foundation
+
+struct HighScores {
+    
+    var topScores: [String]
+    
+    func appendHighScore(score: Int) {
+        
+        
+    }
+    
+    
+}
