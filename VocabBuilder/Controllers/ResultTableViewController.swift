ca587aef2f34e461b12f5262f718288f3d9ce61e
Guillermo Ignacio Sanchez Lamas <sanguillermo98@gmail.com>
2018-07-20 12:32:24 -0700

    Finished thesaurus getter.
    - Updates the thesaurus by showing only words that have synonyms

--- VocabBuilder/Controllers/ResultTableViewController.swift
+++ VocabBuilder/Controllers/ResultTableViewController.swift
@@ -12,7 +12,7 @@ class ResultTableViewController: UIViewController {
     
     var score: Int = 0
     var wordsUsed: [String] = []
-    var wordsAndSynonyms: [String: String] = [:]
+    var synonyms: [String: String] = [:]
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -28,33 +28,6 @@ class ResultTableViewController: UIViewController {
         getSynonyms()
         
     }
-
-    
-    func getSynonyms() {
-        print("Testing synonyms")
-        
-        for word in wordsUsed {
-            var synonymText: String = ""
-            Thesaurus.getSynonymsFor(word: word.lowercased()) { (synonymsArray) in
-                DispatchQueue.main.sync {
-                    for synonym in synonymsArray {
-                        if synonym == synonymsArray[synonymsArray.count - 1] {
-                            synonymText += synonym
-                        } else {
-                            synonymText += synonym +  ", "
-                        }
-                    }
-                    print(synonymText)
-                    self.wordsAndSynonyms[word] = synonymText
-                    self.tableView.reloadData()
-                    
-                }
-            }
-        }
-    }
-    
-
-
 }
 
 
@@ -68,8 +41,43 @@ extension ResultTableViewController: UITableViewDelegate, UITableViewDataSource 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ResultTableViewCell", for: indexPath) as! ResultTableViewCell
         cell.wordTitleLabel.text = wordsUsed[indexPath.row]
-        cell.descriptionTitleLabel.text = wordsAndSynonyms[wordsUsed[indexPath.row]]
+        cell.descriptionTitleLabel.text = synonyms[wordsUsed[indexPath.row]]
         return cell
+    }
+    
+    
+}
+
+
+// Synonym getter
+
+extension ResultTableViewController {
+    
+    func getSynonyms() {
+        //        print("Testing synonyms")
+        
+        for word in wordsUsed {
+            var synonymText: String = ""
+            Thesaurus.getSynonymsFor(word: word.lowercased()) { (synonymsArray) in
+                DispatchQueue.main.sync {
+                    for synonym in synonymsArray {
+                        if synonym == synonymsArray[synonymsArray.count - 1] {
+                            synonymText += synonym
+                        } else {
+                            synonymText += synonym +  ", "
+                        }
+                    }
+                    if synonymText != "" {
+                        self.synonyms[word] = synonymText
+                    } else {
+                        let wordToRemove = self.wordsUsed.index(of: word)!
+                        self.wordsUsed.remove(at: wordToRemove)
+                    }
+                    
+                    self.tableView.reloadData()
+                }
+            }
+        }
     }
     
     
